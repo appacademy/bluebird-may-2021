65621e6d989be1176efbe985ad365d59122cd409
jhnegbrt <jhnegbrt@gmail.com>
2021-06-16 12:18:36 -0700

    adds routes and controllers

---
+++ app/controllers/chirps_controller.rb
@@ -0,0 +1,45 @@
+class ChirpsController < ApplicationController
+
+  def index
+    @chirps = Chirp.all
+    render json: @chirps
+  end
+
+  def show
+    debugger
+    @chirp = Chirp.find(params[:id])
+    render json: @chirp
+  end
+
+  def create
+    debugger
+    @chirp = Chirp.new(chirp_params)
+    if @chirp.save
+      # redirect_to chirp_url(@chirp.id)
+      # redirect_to "localhost:3000/chirps/#{@chirp.id}"
+      redirect_to chirp_url(@chirp)
+    else
+      render json: @chirp.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @chirp = Chirp.find(params[:id])
+    if @chirp.update(chirp_params)
+      redirect_to chirp_url(@chirp)
+    else
+      render json: @chirp.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @chirp = Chirp.find(params[:id])
+    @chirp.destroy
+    redirect_to chirps_url
+  end
+
+  def chirp_params
+    params.require(:chirp).permit(:author_id, :body)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Chirp Routes
+  # get "/chirps", to: "chirps#index"
+  # post "/chirps", to: "chirps#create"
+  # get "/chirps/:id", to: "chirps#show"
+  # patch "/chirps/:id", to: "chirps#update"
+  # delete "/chirps/:id", to: "chirps#destroy"
+
+  resources :chirps, except: [:new, :edit]
+
+
 end
